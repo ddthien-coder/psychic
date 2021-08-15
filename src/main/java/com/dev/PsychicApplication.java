354b44a3da92228df7b3ae9cc5570ad0e2847681
okrabot <jungjihoonkr97@gmail.com>
2021-08-15 20:14:46 +0700

    init project

---
+++ src/main/java/com/dev/PsychicApplication.java
@@ -0,0 +1,13 @@
+package com.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychicApplication.class, args);
+	}
+
+}
